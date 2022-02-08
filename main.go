2815af93216b59c0dc6b086acf589c6c443da745
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-08 11:15:05 -0600

    initial error checking check that user input something define userInput var create switch statement

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Give me the magnitude of the earthquake.")
+	}
+
+	userInput := os.Args[1]
+
+	switch m, _ := strconv.ParseFloat(userInput, 64); {
+	case m >= 10:
+		fmt.Printf("%.2f is massive")
+	case m >= 8:
+		fmt.Printf("%.2f is great")
+
+	}
 }
