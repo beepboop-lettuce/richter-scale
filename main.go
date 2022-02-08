e5b8649be55c5cfc2fc27dad997c3ec75461b6be
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-08 11:23:26 -0600

    switch statement with all desired cases

--- main.go
+++ main.go
@@ -15,9 +15,24 @@ func main() {
 
 	switch m, _ := strconv.ParseFloat(userInput, 64); {
 	case m >= 10:
-		fmt.Printf("%.2f is massive")
+		fmt.Printf("%.2f is massive", m)
 	case m >= 8:
-		fmt.Printf("%.2f is great")
-
+		fmt.Printf("%.2f is great", m)
+	case m >= 7:
+		fmt.Printf("%.2f is major", m)
+	case m >= 6:
+		fmt.Printf("%.2f is strong", m)
+	case m >= 5:
+		fmt.Printf("%.2f is moderate", m)
+	case m >= 4:
+		fmt.Printf("%.2f is light", m)
+	case m >= 3:
+		fmt.Printf("%.2f is minor", m)
+	case m >= 2:
+		fmt.Printf("%.2f is very minor", m)
+	case m < 2:
+		fmt.Printf("%.2f is micro", m)
+	default:
+		fmt.Println("I couldn't get that, sorry.")
 	}
 }
